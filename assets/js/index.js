8435c3cfe5a7d831df38252bf78cf72cbfccda4f
Hans Aguilar <hans.christian.aguilar@gmail.com>
2022-05-12 14:55:51 -0500

    Minor tweaks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('Hello, world.');
+
+const main = document.querySelector('main');
+const url = 'https://images.unsplash.com/photo-1605142859862' + 
+			'-978be7eba909?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8M' + 
+			'HxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit' +
+			'=crop&w=1170&q=80';
+function setBackground() {
+	main.style.background = `#fff url(${url}) center center / cover no-repeat`;
+}
+
+setBackground();
